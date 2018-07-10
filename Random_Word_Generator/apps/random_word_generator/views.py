df1eed1fab198202505f88c89c48b84d63f84288
Kyle <kylehawk2@gmail.com>
2018-07-10 09:22:47 -0700

    Forth Commit

--- Random_Word_Generator/apps/random_word_generator/views.py
+++ Random_Word_Generator/apps/random_word_generator/views.py
@@ -5,15 +5,15 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.utils.crypto import get_random_string
 
 def index(request):
-    # if "count" not in request.session:
-    #     request.session["count"] = 0
-    # if "word" not in request.session:
-    #     request.session["word"] = ""
+    if "count" not in request.session:
+        request.session["count"] = 0
+    if "word" not in request.session:
+        request.session["word"] = ""
     return render(request,'random_word_generator/index.html')
 
 def random_word(request):
     request.session["word"] = get_random_string(length=14, allowed_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    # request.session["count"] += 1
+    request.session["count"] += 1
     return redirect('/')
     # response = "Random Word Route is Working!"
     # return HttpResponse(response)
