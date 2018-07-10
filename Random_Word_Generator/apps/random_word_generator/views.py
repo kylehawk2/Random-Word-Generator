20c325bf527115319fad072f9a402ea779f1511b
Kyle <kylehawk2@gmail.com>
2018-07-10 00:19:31 -0700

    First Commit

---
+++ Random_Word_Generator/apps/random_word_generator/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
+
+def index(request):
+    if "count" not in request.session:
+        request.session["count"] = 0
+    if "word" not in request.session:
+        request.session["word"] = ""
+    return render(request,'random_word_generator/index.html')
+
+def random_word(request):
+    request.session["word"] = get_random_string(length=14, allowed_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    request.session["count"] += 1
+    return redirect('/')
+
+def reset(request):
+    del request.session['word']
+    del request.session['count']
+    return redirect('/')
+    
+    
